229465229215ea0d90649b6e152fc3303cbf68b4
X Garder <apelcinchik1000@gmail.com>
2023-10-12 19:22:52 +0300

    Fixed tasks 2 and 3

--- Lection 1/Task3/main.py
+++ Lection 1/Task3/main.py
@@ -1,20 +1,12 @@
-def ctof(celsius):
-    return (celsius * 9/5) + 32
-def ftoc(fahrenheit):
-    return (fahrenheit - 32) * 5/9
+vibiray = input("choose 1 or 2 for convertation: \n1. Celsius to Farenheit \n2. Farenheit to Celsius\n")
 
-print("Choose type of convertation:")
-print("1. Celsius to Farenheit")
-print("2. Farenheit to Celsius")
-while True:
-    vibiray = input("choose 1 or 2 for convertation: ")
-
-    if vibiray in ('1', '2'):
-        if vibiray == '1':
-            celsius = float(input("Enter celsius: "))
-            fahrenheit = ctof(celsius)
-            print("Farenheit:", fahrenheit)
-        elif vibiray == '2':
-            fahrenheit = float(input("Enter farenheit: "))
-            celsius = ftoc(fahrenheit)
-            print("Celsius:", celsius)
+if vibiray == "1":
+         celsius = float(input("Enter celsius: "))
+         fahrenheit = (celsius * 9/5) + 32
+         print("Farenheit:", fahrenheit)
+elif vibiray == '2':
+        fahrenheit = float(input("Enter farenheit: "))
+        celsius = (fahrenheit - 32) * 5/9
+        print("Celsius:", celsius)
+else:
+        print("Enter 1 or 2!")
