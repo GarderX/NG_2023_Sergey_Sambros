e76e88968b32af9d48c7f49770c50d17b7414326
X Garder <apelcinchik1000@gmail.com>
2023-11-21 10:22:08 +0200

    Lection 2: Task 3 & Task 4 completed

--- Lection2/Task3/main.py
+++ Lection2/Task3/main.py
@@ -1,9 +1,18 @@
-elem = int(input("Enter the number to count until: "))
-el = 0
-print ("_" * 5)
-while elem > 0:  
-  el += 1
-  print (" ", el)
-  if elem == el:
-    break
-print ("_" * 5)
+
+num = int(input("Enter the number:  "))
+
+list = []
+for i in range(1, num + 1):
+    chisla = [j for j in range(1, i + 1) if i % j == 0]
+    list.append([i, chisla])
+
+print("---" * 41)
+
+print("Numbers: ", end="")
+for kilk in list:
+    print(f"{kilk[0]},", end=" ")
+
+simple_numbers = [kilk[0] for kilk in list if len(kilk[1]) == 2]
+print("\n", "---" * 40,  (f"\nDeliteli: {kilk[1]}\n"), "---" * 40 )
+
+print("Prostie chisla: ", simple_numbers, "\n", "---" * 40)
--- Lection2/Task4/main.py
+++ Lection2/Task4/main.py
@@ -1,15 +1,4 @@
-from flask import Flask 
-
-servak = Flask ("Lesson_5")
-
-@servak.route("/")
-def index():
-    cities = ["Hg", "adaw", 3215, "aw"]
-    return cities
-
-
-@servak.route("/sup")
-def index2():
-    return "sup"
-
-servak.run(host = "0.0.0.0", port=8080)
+vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
+enter_string = input(str("Input text: "))
+glasn = [char for char in enter_string if char in vowels]
+print("Vowels in the string:", glasn)
